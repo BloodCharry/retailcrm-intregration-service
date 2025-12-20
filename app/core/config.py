5473a389c3c05996d41393e42da94c5ceb3a93c8
BloodCharry <vladvorobei1994@gmail.com>
2025-12-21 00:49:09 +0300

    add optional enviroment for docker

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,6 +10,14 @@ class Settings(BaseSettings):
     test_offer_id: int | None = None
     # для доступа к FastAPI сервису
     api_key: str
+
+    prometheus_port: int | None = None
+    grafana_port: int | None = None
+    grafana_admin_user: str | None = None
+    grafana_admin_password: str | None = None
+    grafana_log_level: str | None = None
+    grafana_anonymous_enabled: bool | None = None
+    grafana_anonymous_role: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- app/main.py
+++ app/main.py
@@ -2,15 +2,13 @@ from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.v1.routers import customers, orders, payments
 from app.core.logging import logger, setup_logging
 from app.core.middleware import LoggingMiddleware
 from app.core.request_id import RequestIDMiddleware
 from app.core.security import get_api_key
-
-from prometheus_fastapi_instrumentator import Instrumentator
-
 
 setup_logging()
 
